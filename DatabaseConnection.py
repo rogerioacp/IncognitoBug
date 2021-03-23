517f61eb957e1216b04902b8f412ae1703b0d565
Rogério Pontes <rogerio.pontes@pm.me>
2021-03-23 18:35:59 +0100

    Update DatabaseConnection.py

--- DatabaseConnection.py
+++ DatabaseConnection.py
@@ -1,7 +1,7 @@
     import mysql.connector  
       
     #Create the connection object   
-    myconn = mysql.connector.connect(host = "localhost", user = "root",passwd = "google")  
+    myconn = mysql.connector.connect(host = "0.0.0.0", user = "administrator", passwd = "1234567")  
       
     #printing the connection object   
     print(myconn)  
