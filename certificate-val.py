095e25cd90f0aad67278172ff90deb6a05b28d29
Rogério Pontes <rogerio.pontes@pm.me>
2021-03-23 17:47:27 +0100

    Create certificate-val.py

---
+++ certificate-val.py
@@ -0,0 +1,20 @@
+import requests as req
+import requests
+
+some_url = "https://example.com"
+
+# ok:disabled-cert-validation
+r = req.get(some_url, stream=True)
+# ok:disabled-cert-validation
+r = requests.post(some_url, stream=True)
+
+# ruleid:disabled-cert-validation
+r = req.get(some_url, stream=True, verify=False)
+# ruleid:disabled-cert-validation
+r = requests.post(some_url, stream=True, verify=False)
+
+fail_url = "https://autoscout24.com"
+
+
+# ruleid:disabled-cert-validation
+r = requests.post(fail_url, verify=False, stream=True)
